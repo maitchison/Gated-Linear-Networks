3600a899ba541a6c28da818ebd842dc0dc915d17
Matthew Aitchison <matthew.aitchison@anu.edu.au>
2020-07-22 10:37:33 +1000

    added logging

--- layer.py
+++ layer.py
@@ -4,6 +4,66 @@ import torch.distributions as tdist
 
 normal_distribution = tdist.Normal(torch.tensor([0.0]), torch.tensor([0.1]))
 
+class GMN_Layer_Vectorized():
+    """ Vectorized implementation of GMN_layer """
+
+    def __init__(self, in_features, num_nodes, side_info_size, num_contexts):
+        """
+        :param in_features: int, number of input features, i.e. nodes from previous layer
+        :param num_nodes: int, number of nodes in this layer
+        :param side_info_size: int, length of side channel info, i.e. features
+        :param num_contexts: int, number of context planes to use, number of actual contexts will be 2^{this number}
+        """
+
+        self.in_features = in_features + 1 # +1 for bias
+        self.context_dim = 2 ** num_contexts
+        self.num_nodes = num_nodes
+
+        # generate vectorized
+        self.w = torch.zeros(self.num_nodes, self.context_dim, self.in_features)
+        self.context_vectors = torch.Tensor(
+            [[normal_distribution.sample([side_info_size]).view(-1) for i in range(num_contexts)] for _ in range(self.num_nodes)]
+        )
+        self.context_biases = torch.zeros(self.num_nodes, self.num_contexts)
+
+        self.bias = math.e / (math.e + 1)
+
+    def __call__(self, z, p):
+        return self.forward(p, z)
+
+    def get_context(self, x):
+        ret = 0
+        for i in range(self.num_contexts):
+            if torch.dot(x, self.context_vectors[i]) >= self.context_biases[i]:
+                ret = ret + 2 ** i
+        return ret
+
+    def forward(self, z, p):
+        if not p is None:
+            p_hat = torch.cat((torch.as_tensor([self.bias]), p))
+        else:
+            #Forward with random base probabilities
+            p_hat = torch.cat((torch.as_tensor([self.bias]), 0.5 * torch.ones(self.in_features)))
+
+        #[self.nodes[i].forward(p_hat, z) for i in range(len(self.nodes))]
+        #context = self.get_context(z)
+        #return torch.sigmoid(torch.dot(self.w[context], GM_Node.logit(p))), p, context
+
+        pass
+
+    def backward(self, forward, target, learning_rate, hyper_cube_bound = 200):
+        #forward is an array with each element being a tuple (output, p_hat, context)
+        loss = []
+
+        pass
+
+        #for i in range(len(self.nodes)):
+        #    loss.append(self.nodes[i].backward(forward[i][0], target, forward[i][1], forward[i][2], learning_rate, hyper_cube_bound=hyper_cube_bound))
+
+        return loss
+
+
+
 class GMN_layer():
     
     def __init__(self, in_features, num_nodes, side_info_size, num_contexts):
--- train.py
+++ train.py
@@ -4,14 +4,54 @@ import torchvision.datasets as datasets
 import model as M
 import time
 import argparse
+import pickle
+from scipy.ndimage import interpolation
+
 
 # start
 # 1.8 updates per second
 
+def moments(image):
+    # From https://fsix.github.io/mnist/Deskewing.html
+    c0,c1 = np.mgrid[:image.shape[0],:image.shape[1]] # A trick in numPy to create a mesh grid
+    totalImage = np.sum(image) #sum of pixels
+    m0 = np.sum(c0*image)/totalImage #mu_x
+    m1 = np.sum(c1*image)/totalImage #mu_y
+    m00 = np.sum((c0-m0)**2*image)/totalImage #var(x)
+    m11 = np.sum((c1-m1)**2*image)/totalImage #var(y)
+    m01 = np.sum((c0-m0)*(c1-m1)*image)/totalImage #covariance(x,y)
+    mu_vector = np.array([m0,m1]) # Notice that these are \mu_x, \mu_y respectively
+    covariance_matrix = np.array([[m00,m01],[m01,m11]]) # Do you see a similarity between the covariance matrix
+    return mu_vector, covariance_matrix
+
+def deskew_image(image):
+    # From https://fsix.github.io/mnist/Deskewing.html
+    c,v = moments(image)
+    alpha = v[0,1]/v[0,0]
+    affine = np.array([[1,0],[alpha,1]])
+    ocenter = np.array(image.shape)/2.0
+    offset = c-np.dot(affine,ocenter)
+    return interpolation.affine_transform(image,affine,offset=offset)
+
+def deskew(data):
+    """ Applies deskew operation to each example and returns the corrected dataset
+        We assume images are 28x28 represented as np arrange of dims [n,28,28] where n is the number of examples.
+    """
+    result = torch.zeros_like(data)
+    for i in range(len(data)):
+        result[i] = torch.tensor(deskew_image(data[i].numpy()))
+    return result
+
 def center(data):
-    """ Center data so that each example has mean 0."""
-    for d in data:
-        d -= d.mean()
+    """
+    Center data so that each example has mean 0.
+    :param data: float tensor of dims [n,28,28]
+    :return: centered float tensor of dims [n,28,28]
+    """
+    result = torch.zeros_like(data)
+    for i in range(len(data)):
+        result[i] = data[i] - data[i].mean()
+    return result
 
 def validate(network, max_samples=None):
     validation_samples = max_samples or len(mnist_val)
@@ -30,7 +70,11 @@ def validate(network, max_samples=None):
             correct = correct + 1
         total = total + 1
 
-    print("Accuracy: {:.2f}%".format(100 * (correct / total)))
+    return 100 * (correct / total)
+
+def save_db(db):
+    with open(db["filename"], 'w') as f:
+        pickle.dump(db, f)
 
 def train():
 
@@ -42,19 +86,37 @@ def train():
 
     training_samples = len(mnist_train)
 
+    db = {}
+    db['val_score'] = []
+    db['time_stamp'] = []
+    db['iteration'] = []
+
     for i in range(training_samples):
-        if (i + 1) % 10 == 0:
-            print("Training: {:.2f}%".format(100 * (i + 1) / training_samples))
+        if (i + 1) % 100 == 0:
+            print(" -training: {:.2f}%".format(100 * (i + 1) / training_samples))
         for j in range(len(network)):
             network[j].train_on_sample(mnist_train[i].view(-1), 1 if j == mnist_train_labels[i] else 0,
                                        min(100 / (i + 1), 0.01))
 
         # quick validation check...
-        if (i + 1) % 100 == 0:
-            validate(network, 25)
+        if i % 1000 == 0:
+            score = validate(network, 100)
+            db['time_stamp'].append(time.time())
+            print(f" -quick check {score:.1f}%")
+            db['val_score'].append(score)
+            db['iteration'].append(i)
 
-    validate(network)
+        if (i + 1) % 10000 == 0:
+            score = validate(network, 1000)
+            print()
+            print(f"Performance: {score:.1f}%")
+            print()
 
+    score = validate(network)
+    print("*"*60)
+    print(f"Final score: {score:.2f}%")
+    print("*"*60)
+    db['final_score'].append(score)
 
 def load_data():
 
@@ -66,12 +128,20 @@ def load_data():
     global mnist_val
     global mnist_val_labels
 
+    print("Loading dataset...")
+
     mnist_train = datasets.MNIST(root=DATA_PATH, train=True, download=DOWNLOAD).data.float() / 255
-    center(mnist_train)
+    mnist_train = center(deskew(mnist_train))
     mnist_train_labels = datasets.MNIST(root=DATA_PATH, train=True, download=DOWNLOAD).targets
     mnist_val = datasets.MNIST(root=DATA_PATH, train=False, download=DOWNLOAD).data.float() / 255
-    center(mnist_val)
+    mnist_val = center(deskew(mnist_val))
     mnist_val_labels = datasets.MNIST(root=DATA_PATH, train=False, download=DOWNLOAD).targets
+
+    n = len(mnist_train)
+    mu = mnist_train.mean()
+    sigma = mnist_train.std()
+
+    print(f" - read {n} images with mu:{mu:.1f} sigma:{sigma:.1f}")
 
     return mnist_train, mnist_train_labels, mnist_val, mnist_val_labels
 
